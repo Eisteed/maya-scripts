0651523b6e88a7ff1ffeff7d0420030f42ad0561
EisBits <a.chypre@gmail.com>
2024-09-05 10:39:40 +0200

    Update FBX_Export_High_Low.py
    Added support to all object hierarchy for selected groups

--- FBX_Export_High_Low.py
+++ FBX_Export_High_Low.py
@@ -14,29 +14,25 @@ for group in selected_groups:
     # I'm used to rename all groups with _GRP suffix, I don't wanna see it in export
     export_name = group.replace("_GRP", "")
 
-    # Select all geometry inside the group
-    cmds.select(group, replace=True)
-    geometry = cmds.listRelatives(allDescendents=True, type="mesh", fullPath=True)
-    cmds.select(geometry, replace=True)
+    cmds.select(group, hierarchy=True)
 
     # Export low version (without smooth mesh)
     low_file_path = os.path.join(export_dir, f"{export_name}_low.fbx")
     mel.eval(f'FBXExport -f "{low_file_path}" -s')
 
-    # Add smooth mesh to all selected geometry
+    mesh_objects = cmds.listRelatives(group, allDescendents=True, type="mesh", fullPath=True)
+    
+    # Add smooth mesh to all mesh objects
     smooth_nodes = []
-    for geo in geometry:
-        smooth_node = cmds.polySmooth(geo, divisions=1)[0]
+    for mesh in mesh_objects:
+        smooth_node = cmds.polySmooth(mesh, divisions=1)[0]
         smooth_nodes.append(smooth_node)
 
     # Export high version (with smooth mesh)
     high_file_path = os.path.join(export_dir, f"{export_name}_high.fbx")
     mel.eval(f'FBXExport -f "{high_file_path}" -s')
 
-    # Remove smooth mesh nodes
     cmds.delete(smooth_nodes)
-
-    # Reselect the original group
     cmds.select(group, replace=True)
 
 print("High/Low Export complete.")
